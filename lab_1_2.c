0ba550d127e1c3696f2431c2bbbcd3d81643e2e8
GrigoriyBykovskiy <37000674+GrigoriyBykovskiy@users.noreply.github.com>
2018-03-17 01:57:29 +0300

    lab_1_1 with files work
    2-st part of my work

---
+++ lab_1_2.c
@@ -0,0 +1,25 @@
+﻿// Программа записывает в файл результат проверки, попала ли точка в заданную область,считывая данные из исходного файла
+#define _CRT_SECURE_NO_WARNINGS
+#include "stdafx.h"
+#include "assert.h"
+void is_hit(float x, float y,FILE *output);
+int main()
+{
+	FILE *input,*output;
+	printf("Programm is running...\n");
+		if ((input = fopen("dots_coordinates.txt", "r")) == NULL) printf("File could not be opened\n");
+		else {
+			output = fopen("are_hit_dots.txt", "w");
+			while (!feof(input)) {
+				float x, y;
+				assert(fscanf(input, "%f %f", &x, &y));// если в файле есть не только числа - вылетим с сообщением об ошибке
+				is_hit(x, y, output);
+			}
+		}
+		printf("Thats all. Please, check out output file are_hit_dots.txt\n");
+	return 0;
+}
+void is_hit(float x, float y, FILE *output) {
+	if ((y - x + 1 <= 0) && ((x - 0.5)*(x - 0.5) + (y - 0.5)*(y - 0.5) >= 2.25)) fprintf(output,"YES\n");
+	else fprintf(output,"NO\n");
+}
